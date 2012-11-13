a8b9f94f33039735f2b1b95fb12bf6838e02d538
Matthew <mvd7793@gmail.com>
2012-11-13 11:03:48 -0600

    Committing pom changes and actiontype changes

--- src/main/java/com/dierkers/schedule/action/ActionType.java
+++ src/main/java/com/dierkers/schedule/action/ActionType.java
@@ -1,9 +1,21 @@
 package com.dierkers.schedule.action;
 
 public class ActionType {
+	// Debug, prints to error log
 	public final static int ERROR_PRINT = 1;
+
+	// Send a Facebook message
 	public final static int FACEBOOK_MESSAGE = 2;
+
+	// Send a SMS
 	public final static int SMS = 3;
+
+	// Make a call
 	public final static int CALL = 4;
+
+	// Send an email (from DreamHost mail servers)
 	public final static int MAIL = 5;
+
+	// Check a class's availability
+	public final static int CLASS_CHECKER = 6;
 }
